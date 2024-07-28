20dc3c914030df9ec4fae91b799947e9b54d2bc3
afayyaz1 <afayyaz1@swarthmore.edu>
2024-07-28 08:23:55 -0400

    navigationLink added to ContentView

---
+++ PowerStation/BatteryDataView.swift
@@ -0,0 +1,18 @@
+//
+//  BatteryDataView.swift
+//  PowerStation
+//
+//  Created by Ahmad Fayyaz on 28/07/2024.
+//
+
+import SwiftUI
+
+struct BatteryDataView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BatteryDataView()
+}
