ea1720ff23a80687493284326303d9f7fb04c579
afayyaz1 <afayyaz1@swarthmore.edu>
2024-07-28 06:55:36 -0400

    added sidebar

---
+++ PowerStation/SidebarView.swift
@@ -0,0 +1,18 @@
+//
+//  SidebarView.swift
+//  PowerStation
+//
+//  Created by Ahmad Fayyaz on 28/07/2024.
+//
+
+import SwiftUI
+
+struct SidebarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SidebarView()
+}
