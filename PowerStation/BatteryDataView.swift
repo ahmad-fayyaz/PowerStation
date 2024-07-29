ad5883cf9f2685115a291d495e0fbfdb2a9cf06b
afayyaz1 <afayyaz1@swarthmore.edu>
2024-07-28 21:25:19 -0400

    dashboard cleanup

--- PowerStation/BatteryDataView.swift
+++ PowerStation/BatteryDataView.swift
@@ -7,16 +7,8 @@ struct BatteryDataView: View {
     @State private var timeToFullCharge: Int?
     @State private var timeToEmpty: Int?
     @State private var isCharging: Bool?
-    @State private var batteryHealth: String?
     @State private var powerSourceState: String?
-    @State private var powerSourceID: Int?
-    @State private var name: String?
-    @State private var optimizedBatteryChargingEngaged: Bool?
-    @State private var transportType: String?
-    @State private var type: String?
-    @State private var current: Int?
-    @State private var lpmActive: Bool?
-    @State private var providesTimeRemaining: Bool?
+    @State private var batteryHealth: String?
     
     var body: some View {
         VStack(alignment: .leading) {
@@ -35,15 +27,13 @@ struct BatteryDataView: View {
                     .padding()
             }
             
-            
             // Time to charge + discharge Widget
             HStack(alignment: .center) {
                 VStack {
                     Label("Time to Full Charge", systemImage: "timelapse")
                     if let timeToFullCharge = timeToFullCharge {
                         ProgressView(value: 0.25)
-                                        .progressViewStyle(.circular)
-                        
+                            .progressViewStyle(.circular)
                         Text(" \(timeToFullCharge) minutes")
                             .font(.title)
                             .fontWeight(.heavy)
@@ -54,7 +44,7 @@ struct BatteryDataView: View {
                     Label("Time to Empty", systemImage: "exclamationmark.warninglight.fill")
                     if let timeToEmpty = timeToEmpty {
                         ProgressView(value: 0.25)
-                                        .progressViewStyle(.circular)
+                            .progressViewStyle(.circular)
                         Text("\(timeToEmpty) minutes")
                             .font(.title)
                             .fontWeight(.heavy)
@@ -63,71 +53,49 @@ struct BatteryDataView: View {
             }
             
             // Is Charging? Widget
-            
             if let isCharging = isCharging {
-                            ZStack {
-                                Rectangle()
-                                    .fill(isCharging ? Color.green : Color.gray)
-                                    .frame(width: 600.0, height: 50.0)
-                                    .cornerRadius(10)
-                                Text("Is Charging: \(isCharging ? "Yes" : "No")")
-                                    .foregroundColor(.white)
-                                    .bold()
-                            }
-                            
-                        }
-            
-            if let batteryHealth = batteryHealth {
-                Text("Battery Health: \(batteryHealth)")
-            }
-            if let powerSourceState = powerSourceState {
-                Text("Power Source State: \(powerSourceState)")
-            }
-            if let powerSourceID = powerSourceID {
-                Text("Power Source ID: \(powerSourceID)")
-            }
-            if let name = name {
-                Text("Name: \(name)")
-            }
-            if let optimizedBatteryChargingEngaged = optimizedBatteryChargingEngaged {
-                Text("Optimized Battery Charging Engaged: \(optimizedBatteryChargingEngaged ? "Yes" : "No")")
-            }
-            if let transportType = transportType {
-                Text("Transport Type: \(transportType)")
-            }
-            if let type = type {
-                Text("Type: \(type)")
-            }
-            if let current = current {
-                Text("Current: \(current) mA")
-            }
-            if let lpmActive = lpmActive {
-                Text("LPM Active: \(lpmActive ? "Yes" : "No")")
-            }
-            if let providesTimeRemaining = providesTimeRemaining {
-                Text("Battery Provides Time Remaining: \(providesTimeRemaining ? "Yes" : "No")")
+                ZStack {
+                    Rectangle()
+                        .fill(isCharging ? Color.green : Color.gray)
+                        .frame(width: 600.0, height: 50.0)
+                        .cornerRadius(10)
+                    Text("Is Charging: \(isCharging ? "Yes" : "No")")
+                        .foregroundColor(.white)
+                        .bold()
+                }
             }
             
+            // Power Source Widget
+            HStack {
+                Label(title: { Text("Power Source") }, icon: { Image(systemName: "bolt.fill") })
+                Spacer()
+                if let powerSourceState = powerSourceState {
+                    Text("\(powerSourceState)")
+                }
+            }
+            
+            
+            // Battery Health Widget
+            HStack {
+                Label(title: { Text("Battery Health") }, icon: { Image(systemName: "heart.fill") })
+                Spacer()
+                if let batteryHealth = batteryHealth {
+                    Text("\(batteryHealth)")
+                }
+            }
+            
+            // Loading State
             if currentCapacity == nil &&
                 timeToFullCharge == nil &&
                 timeToEmpty == nil &&
                 isCharging == nil &&
-                batteryHealth == nil &&
                 powerSourceState == nil &&
-                powerSourceID == nil &&
-                name == nil &&
-                optimizedBatteryChargingEngaged == nil &&
-                transportType == nil &&
-                type == nil &&
-                current == nil &&
-                lpmActive == nil &&
-                providesTimeRemaining == nil {
+                batteryHealth == nil {
                 Text("Loading Battery Data...")
                     .font(.headline)
             }
         }
-        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-        .frame(height: 600.0)
+        .padding()
         .onAppear {
             self.fetchBatteryInfo()
         }
@@ -136,23 +104,15 @@ struct BatteryDataView: View {
     func fetchBatteryInfo() {
         let snapshot = IOPSCopyPowerSourcesInfo().takeRetainedValue()
         let sources = IOPSCopyPowerSourcesList(snapshot).takeRetainedValue() as Array
-
+        
         for ps in sources {
             if let info = IOPSGetPowerSourceDescription(snapshot, ps).takeUnretainedValue() as? [String: Any] {
                 self.currentCapacity = info[kIOPSCurrentCapacityKey as String] as? Int
                 self.timeToFullCharge = info[kIOPSTimeToFullChargeKey as String] as? Int
                 self.timeToEmpty = info[kIOPSTimeToEmptyKey as String] as? Int
                 self.isCharging = info[kIOPSIsChargingKey as String] as? Bool
+                self.powerSourceState = info[kIOPSPowerSourceStateKey as String] as? String
                 self.batteryHealth = info["BatteryHealth"] as? String
-                self.powerSourceState = info["Power Source State"] as? String
-                self.powerSourceID = info["Power Source ID"] as? Int
-                self.name = info["Name"] as? String
-                self.optimizedBatteryChargingEngaged = info["Optimized Battery Charging Engaged"] as? Bool
-                self.transportType = info["Transport Type"] as? String
-                self.type = info["Type"] as? String
-                self.current = info["Current"] as? Int
-                self.lpmActive = info["LPM Active"] as? Bool
-                self.providesTimeRemaining = info["Battery Provides Time Remaining"] as? Bool
             }
         }
     }
